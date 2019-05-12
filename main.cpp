52745a65bc9264971724f1e2ae306688504b1355
TrutnevaOV <trutneva.o@list.ru>
2019-05-12 15:48:08 +0300

    code: печать 16-чной цифры от 0 до 15

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
-
+#include <assert.h>
+#include <cstdint>
 using namespace std;
+
+char nibble_to_hex (uint8_t i)
+{
+    assert(0x0 <= i && i <= 0xf);
+    char symbols []="0123456789abcdef";
+    return symbols[i];
+}
+
+void print_in_hex(uint8_t byte)
+{
+
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    assert(nibble_to_hex(0x0) == '0');
+    assert(nibble_to_hex(0x1) == '1');
+    assert(nibble_to_hex(0x2) == '2');
+    assert(nibble_to_hex(0x3) == '3');
+    assert(nibble_to_hex(0x4) == '4');
+    assert(nibble_to_hex(0x5) == '5');
+    assert(nibble_to_hex(0x6) == '6');
+    assert(nibble_to_hex(0x7) == '7');
+    assert(nibble_to_hex(0x8) == '8');
+    assert(nibble_to_hex(0x9) == '9');
+    assert(nibble_to_hex(0xa) == 'a');
+    assert(nibble_to_hex(0xb) == 'b');
+    assert(nibble_to_hex(0xc) == 'c');
+    assert(nibble_to_hex(0xd) == 'd');
+    assert(nibble_to_hex(0xe) == 'e');
+    assert(nibble_to_hex(0xf) == 'f');
 }
