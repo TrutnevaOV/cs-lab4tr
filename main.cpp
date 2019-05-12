d0d94b4363be4b956b0a9b5e6fdda07ecfbec505
TrutnevaOV <trutneva.o@list.ru>
2019-05-12 16:33:15 +0300

    code: приведение типов

--- main.cpp
+++ main.cpp
@@ -16,6 +16,30 @@ void print_in_hex(uint8_t byte)
          << nibble_to_hex(byte & 0xf);
 }
 
+const uint8_t*
+as_bytes(const void* data)
+{
+    return reinterpret_cast<const uint8_t*>(data);
+}
+
+void
+print_in_hex(const void* data, size_t size)
+{
+    const uint8_t* bytes = as_bytes(data);
+    for (size_t i = 0; i < size; i++)
+    {
+        print_in_hex(bytes[i]);
+        if ((i + 1) % 16 == 0)
+        {
+            cout << '\n';
+        }
+        else
+        {
+            cout << ' ';
+        }
+    }
+}
+
 int main()
 {
     assert(nibble_to_hex(0x0) == '0');
